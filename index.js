9bf2cabe201d57b22ccac405ea22198634de3171
Andrew Mitchell <mitch702@msu.edu>
2017-02-02 16:29:21 -0500

    fix errors

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-//grab input elements
+// grab input elements
 var firstNameInput = document.getElementById('first-name')
 var lastNameInput = document.getElementById('last-name')
 var descriptionInput = document.getElementById('description')
 var emailInput = document.getElementById('email')
 var phoneInput = document.getElementById('phone')
-//grab divs for input
-var rawP = document.getElementById('raw')
-var htmlP = document.getElementById('html')
-
-//grabs values from inputs and updates the preview divs
-var updateAll = function(){
-  //grab all values from input boxes
+// grab divs for input
+var previewParagraph = document.getElementById('raw')
+var rawHTMLParagraph = document.getElementById('html')
+// grabs values from inputs and updates the preview divs
+var updateAll = function () {
+  // grab all values from input boxes
   var firstName = firstNameInput.value
   var lastName = lastNameInput.value
   var description = descriptionInput.value
   var email = emailInput.value
   var phone = phoneInput.value
-  //string to display preview
-  var string = '<h1>Hi, my name is '+ firstName + ' ' + lastName + '!</h1>' +
-    '<p>'+ description + '</p>' +
+  // string to display preview
+  var string =
+    '<h1>Hi, my name is ' + firstName + ' ' + lastName + '!</h1>' +
+    '<p>' + description + '</p>' +
     '<p>If you\'re interested in a date, you can email me at ' +
-    '<a href="mailto:' + email + '"target="_blank">' + email + '</a>'  +
-    ' or give me a call at ' +
-    '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>.</p>'
-  //show raw html and preview
-  rawP.textContent = string
-  htmlP.innerHTML = string
+      '<a href="mailto:' + email + '"target="_blank">' + email + '</a>' +
+      ' or give me a call at ' +
+      '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>.' +
+    '</p>'
+  // show raw html and preview
+  previewParagraph.textContent = string
+  rawHTMLParagraph.innerHTML = string
 }
-
-//add listeners to all inputs with updateAll function
+// add listeners to all inputs with updateAll function
 lastNameInput.addEventListener('input', updateAll)
 firstNameInput.addEventListener('input', updateAll)
 descriptionInput.addEventListener('input', updateAll)
